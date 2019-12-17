48fe0664933dc428eae3c81ae0dbe3cf6cc1aba3
Darren SooHoo <dsoohoo@MossBeachHomes.com>
2019-12-17 11:52:56 -0800

    3 AddBorder complete

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,17 @@
 function addBorder(picture: string[]): string[] {
     const lengthOfWall = picture[0].length + 2;
+    let horizontalBorder = '';
+    for(let i = 0; i < lengthOfWall; i++) {
+        horizontalBorder += '*';
+    };
+    for(let i = 0; i < picture.length; i++){
+        picture[i] = '*' + picture[i];
+        picture[i] += '*';
+    }
+        
+    picture.push(horizontalBorder);
+    picture.unshift(horizontalBorder);
+    return picture;
 }
 
 console.log(addBorder(["abc", "ded"]));

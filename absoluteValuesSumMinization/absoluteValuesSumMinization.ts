6115848f4eed3050dd6cb392e0cb6d4161204f12
Darren SooHoo <darsoohoo@gmail.com>
2019-12-17 00:27:21 -0800

    populate

---
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -0,0 +1,9 @@
+function absoluteValuesSumMinimization(a: number[]): number {
+
+
+}
+
+// console.log(absoluteValuesSumMinimization([2, 4, 7]));
+// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8])); 

87f618335a67037d4e926bc3a964be8c9d3eb2d6
Darren SooHoo <dsoohoo@MossBeachHomes.com>
2019-12-17 16:55:54 -0800

    adjacentElementsProduct: incomplete

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,12 @@
 function adjacentElementsProduct(inputArray: number[]): number {
 
+    let largest;
+    for(let i = 0)
+
+
+
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+
+
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
